265cd4a7ed91ae19844ab80327616691a70764ad
Pablozzz19 <72037798+Pablozzz19@users.noreply.github.com>
2021-03-22 11:02:49 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tech/Account/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.tech.Account;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tech.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
