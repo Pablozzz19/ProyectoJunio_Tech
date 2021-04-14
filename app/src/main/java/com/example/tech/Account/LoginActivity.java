d497f40e5e457458c31133f466d6808f3d7ebbe2
Pablozzz19 <72037798+Pablozzz19@users.noreply.github.com>
2021-04-14 21:02:25 +0200

    Pantalla Login

--- app/src/main/java/com/example/tech/Account/LoginActivity.java
+++ app/src/main/java/com/example/tech/Account/LoginActivity.java
@@ -2,15 +2,41 @@ package com.example.tech.Account;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.tech.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btnLogin;
+    TextView tvRegistrarse;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        tvRegistrarse = (TextView)findViewById(R.id.tvRegistrarse);
+
+        /*btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),  CrearCuentaActivity.class);
+                startActivity(intent);
+            }
+        });*/
+
+        tvRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),  CrearCuentaActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
